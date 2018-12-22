2693bda2c36256cc98ca5eb38c6dc6f7094a6e95
Hasee <1434756304@qq.com>
2018-12-22 14:55:51 +0800

    通用Mapper集成

--- src/main/java/com/wondertek/mybatis/MybatisApplication.java
+++ src/main/java/com/wondertek/mybatis/MybatisApplication.java
@@ -1,13 +1,8 @@
 package com.wondertek.mybatis;
 
-import com.wondertek.mybatis.plugin.FirstPlugin;
-import org.apache.ibatis.plugin.Interceptor;
-import org.apache.ibatis.plugin.Intercepts;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
