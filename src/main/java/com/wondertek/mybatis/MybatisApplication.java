d42cd4a683e1260fac83324968ba24a9f040abd8
Hasee <1434756304@qq.com>
2019-03-03 18:49:18 +0800

    bug修复

--- src/main/java/com/wondertek/mybatis/MybatisApplication.java
+++ src/main/java/com/wondertek/mybatis/MybatisApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 @MapperScan("com.wondertek.mybatis.mapper")
 public class MybatisApplication {
     public static void main(String[] args) {
